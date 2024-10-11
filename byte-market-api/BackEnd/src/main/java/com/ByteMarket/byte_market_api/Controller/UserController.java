ef7d5bf3a5d6112352553579c1d14936fcf6a34e
bean_sint <123951763+beansint@users.noreply.github.com>
2024-10-11 20:47:35 +0800

    Need clarification on delete and update, Add and Get complete

--- byte-market-api/BackEnd/src/main/java/com/ByteMarket/byte_market_api/Controller/UserController.java
+++ byte-market-api/BackEnd/src/main/java/com/ByteMarket/byte_market_api/Controller/UserController.java
@@ -1,12 +1,40 @@
 package com.ByteMarket.byte_market_api.Controller;
 
+import com.ByteMarket.byte_market_api.Entity.UserEntity;
 import com.ByteMarket.byte_market_api.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/user")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     UserService userService;
 
+    @PostMapping("/addUser")
+    public UserEntity addUser(@RequestBody UserEntity user){
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/getAllUser")
+    public List<UserEntity> getAllUser(){
+        return userService.getAllUser();
+    }
+
+    @GetMapping("/getUserById/{id}")
+    public UserEntity getUserById(@PathVariable int id){
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/getUserById/{id}")
+    public UserEntity updateUser(@PathVariable int id, @RequestBody UserEntity newUser){
+        return userService.updateUser(id, newUser);
+    }
+
+//    @DeleteMapping("/deleteUser/{id}")
+//    public UserEntity deleteUser(@PathVariable int id){
+//        return userService.deleteUser(id);
+//    }
 }
