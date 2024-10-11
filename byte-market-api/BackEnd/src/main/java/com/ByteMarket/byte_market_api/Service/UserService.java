91383fbf9acf2f3b453e6f9e83747d0ed10242d8
Danrave Cabahug Keh <danravekeh123@gmail.com>
2024-10-11 20:03:53 +0800

    Implementation of Service

--- byte-market-api/BackEnd/src/main/java/com/ByteMarket/byte_market_api/Service/UserService.java
+++ byte-market-api/BackEnd/src/main/java/com/ByteMarket/byte_market_api/Service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -18,14 +19,33 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    //Get User By ID
+    //Get User By ID / Find User By ID
     public UserEntity getUserById(int id){
         return userRepository.findById(id).get();
     }
 
-    //Post User
-    public UserEntity postUser(UserEntity user){
+    //Post User / Insert User
+    public UserEntity insertUser(UserEntity user){
         return userRepository.save(user);
     }
 
+    //Put User / Update User
+    public UserEntity updateUser(int id, UserEntity newUser){
+        UserEntity userEntity = userRepository.findById(id).get();
+        userEntity.setUsername(newUser.getUsername());
+        userEntity.setPassword(newUser.getPassword());
+        userEntity.setEmail(newUser.getEmail());
+        userEntity.setAddress(newUser.getAddress());
+        userEntity.setCity(newUser.getCity());
+        userEntity.setCountry(newUser.getCountry());
+        userEntity.setPhonenumber(newUser.getPhonenumber());
+        userEntity.setDateofbirth(newUser.getDateofbirth());
+
+        return userRepository.save(userEntity);
+    }
+
+    //Delete User
+    public void deleteUser(int id){
+        userRepository.deleteById(id);
+    }
 }
