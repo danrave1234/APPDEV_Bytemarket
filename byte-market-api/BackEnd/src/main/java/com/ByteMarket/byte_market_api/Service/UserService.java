86dc217bd0343b8adbf270e6ce7a13737a9dcb16
Danrave Cabahug Keh <danravekeh123@gmail.com>
2024-10-11 19:18:43 +0800

    Ge implement nako ang basic structures sa CRUD

--- byte-market-api/BackEnd/src/main/java/com/ByteMarket/byte_market_api/Service/UserService.java
+++ byte-market-api/BackEnd/src/main/java/com/ByteMarket/byte_market_api/Service/UserService.java
@@ -1,13 +1,31 @@
 package com.ByteMarket.byte_market_api.Service;
 
 
+import com.ByteMarket.byte_market_api.Entity.UserEntity;
 import com.ByteMarket.byte_market_api.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+
+    //Get ALL User
+    public List<UserEntity> getUser(){
+        return userRepository.findAll();
+    }
+
+    //Get User By ID
+    public UserEntity getUserById(int id){
+        return userRepository.findById(id).get();
+    }
+
+    //Post User
+    public UserEntity postUser(UserEntity user){
+        return userRepository.save(user);
+    }
 
 }
