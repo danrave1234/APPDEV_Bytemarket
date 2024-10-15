c279531c18d09aa89450bdabef70c92d9e2d9c5a
Danrave Cabahug Keh <danravekeh123@gmail.com>
2024-10-16 01:14:18 +0800

    Created and experiemented with jsx files using routes.

---
+++ byte-market-api/FrontEnd/frontend/src/LandingPage.jsx
@@ -0,0 +1,14 @@
+import {useNavigate} from "react-router-dom";
+
+export default function LandingPage() {
+    const navigate = useNavigate();
+    const handleRegister = () => {
+        navigate("/registerpage");
+    };
+    return (
+        <>
+            <h1>Hello, this is our landing page</h1>
+            <button onClick={handleRegister}><b>REGISTER NOW!!!!</b></button>
+        </>
+    )
+}
