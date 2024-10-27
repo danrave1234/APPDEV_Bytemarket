38101a0a2beac0b6a28e64d6e487e6c39012da90
Danrave Cabahug Keh <danravekeh123@gmail.com>
2024-10-27 20:16:44 +0800

    Dont with admin dashboard

--- byte-market-api/FrontEnd/frontend/vite.config.js
+++ byte-market-api/FrontEnd/frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'C:/Users/Danrave/Desktop/School/APPDEV_Final Project/byte-market-api/BackEnd/src/main/resources/static',
+    // Set the output directory to the Spring Boot static folder
+    emptyOutDir: true,
+  }
 })
