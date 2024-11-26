558bd3a124776666fc1e7f7f0327736214225e75
Danrave Cabahug Keh <danravekeh123@gmail.com>
2024-11-26 11:14:53 +0800

    Logout Modal is popped up when user clicks login when its already logged in.

---
+++ byte-market-api/FrontEnd/frontend/src/components/LogoutModal.jsx
@@ -0,0 +1,35 @@
+import {useAuth} from "./AuthProvider.jsx";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import './LogoutModal.css';
+
+const LogoutModal = ({show, closeModal}) => {
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        logout();
+        navigate("/");
+        closeModal();
+    };
+    const handleClose = () =>{
+        closeModal();
+    }
+    if (!show) return null;
+    return (
+        <>
+            <div className="logout-modal-overlay">
+                <div className="logout-modal-box">
+                    <h1>Logout</h1>
+                    <p>Are you sure you want to logout?</p>
+                    <button onClick={handleClose}>
+                        Close
+                    </button>
+                    <button onClick={handleLogout}>
+                        Logout
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+}
+export default LogoutModal;
